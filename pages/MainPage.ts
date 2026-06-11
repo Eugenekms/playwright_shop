84b097e6a7ff5f5b85663bd10a190514e60ee4c5
Eugene <eakvyatkovsky@gmail.com>
2026-06-11 19:26:02 +0400

    test: add networkidle wait and check all filtered cards

--- pages/MainPage.ts
+++ pages/MainPage.ts
@@ -6,16 +6,22 @@ export class MainPage {
     readonly searchInput: Locator;
     readonly searchCountResult: Locator;
     readonly signInButton: Locator;
+    readonly handToolHammer: Locator;
+    readonly cardProductName: Locator;
 
     constructor(page: Page) {
         this.page = page;        
         this.searchInput = page.getByPlaceholder('Search');
         this.searchCountResult = page.getByTestId('search-result-count');
         this.signInButton = page.getByRole('link', { name: 'Sign in'})
+        this.handToolHammer = page.getByRole('checkbox', { name: 'Hammer'});
+        this.cardProductName = page.getByTestId('product-name');
     }
     
     async searchForProduct(productName: string) {
         await this.searchInput.fill(productName);
         await this.page.keyboard.press('Enter'); 
     }
+    
+    
 }
--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -8,5 +8,21 @@ test ('check search tools for main page', async({page}) => {
     await page.goto('https://practicesoftwaretesting.com');
     await mainPage.searchForProduct('drill');
     
-    await expect(mainPage.searchCountResult).toHaveText(/.*drill.*/)
+    await expect(mainPage.searchCountResult).toHaveText(/.*drill*/);
 });
+
+test ('single checkbox filter', async({page}) => {
+
+    const mainPage = new MainPage(page);
+    await page.goto('https://practicesoftwaretesting.com');
+    await mainPage.handToolHammer.check();
+    await expect(mainPage.cardProductName.first()).toBeVisible();
+    await page.waitForLoadState('networkidle')
+    const allCards = await mainPage.cardProductName.all();
+
+    for (const card of allCards) {
+        await expect(card).toContainText('Hammer', { ignoreCase: true});
+        console.log('check')
+    }
+    
+})
