d755049644eb31d2984bd0961c39fe57d92f7015
Eugene <eakvyatkovsky@gmail.com>
2026-06-16 22:41:23 +0400

    ci: configure github actions and add api test

--- tests/products-api.spec.ts
+++ tests/products-api.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, request } from '@playwright/test';
 
 test ('request main page', async ({ request }) => {
 
@@ -9,4 +9,18 @@ test ('request main page', async ({ request }) => {
     expect(body.data.length).toBeGreaterThan(0);
     expect(body.data[0].id).toBeTruthy();
     
+});
+
+test ('login via api', async ({request}) => {
+
+    const response = await request.post('https://api.practicesoftwaretesting.com/users/login', {
+        data: {
+            email: process.env.CUSTOMER_EMAIL!,
+            password: process.env.CUSTOMER_PASSWORD!
+        }
+    });
+
+    expect(response.status()).toBe(200);
+    const body = await response.json();
+    expect(body.access_token).toBeDefined();
 })
