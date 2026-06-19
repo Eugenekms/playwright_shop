34655de28a1976e37a676753d6fee0cc10e5501a
Eugene <eakvyatkovsky@gmail.com>
2026-06-19 22:23:30 +0400

    refactor: implement global state-based authentication and update checkout flow

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -1,14 +1,21 @@
-import { test as setup, expect, request } from '@playwright/test';
+import { test as setup, expect } from '@playwright/test';
 
 const authFile = 'playwright/.auth/user.json'
 
-setup('authenticate', async ({ page }) => {
-    
+setup('authenticate safely', async ({ page }) => {
+    // 1. Идем на страницу логина
     await page.goto('https://practicesoftwaretesting.com/auth/login');
+    
+    // 2. Вводим данные
     await page.getByTestId('email').fill(process.env.CUSTOMER_EMAIL!);
     await page.getByPlaceholder('password').fill(process.env.CUSTOMER_PASSWORD!);
-    await page.getByRole('button', { name: 'Login'}).click();
     
-    await expect(page.getByRole('heading', { name: 'My account'})).toBeVisible();    
+    // 3. Жмем кнопку и ОЧЕНЬ ВАЖНО: ждем завершения навигации
+    await Promise.all([
+        page.waitForURL('**/account'), // Ждем перехода в аккаунт
+        page.getByRole('button', { name: 'Login' }).click()
+    ]);
+
+    // 4. Принудительно сохраняем всё, включая LocalStorage
     await page.context().storageState({ path: authFile });
 });
