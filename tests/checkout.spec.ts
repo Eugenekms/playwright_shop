5bb0acd0a2d627dbfb97aa02564e7d5755bbcfa2
Eugene <eakvyatkovsky@gmail.com>
2026-06-27 16:51:03 +0400

    feat: add end-to-end purchase flow test and fix order confirmation locator

--- tests/checkout.spec.ts
+++ tests/checkout.spec.ts
@@ -1,37 +1,6 @@
 import { faker } from '@faker-js/faker';
 import { test, expect } from '../fixtures/baseTest';
 
-test('check buy product', async ({ mainPage, checkoutPage, productPage }) => {
-    const shippingAddress = {
-        country: 'United States of America (the)',
-        city: 'New York',
-        state: 'NY',
-        postalCode: '10001',
-        street: '5th Avenue',
-        houseNumber: '123',
-    };
-
-    await mainPage.open();
-    await mainPage.choseInStock();
-    await productPage.addToCartButton.click();
-    await mainPage.cartLink.click();
-
-    await checkoutPage.proceedToCheckoutButton1.click();
-
-    await checkoutPage.proceedToCheckoutButton2.click();
-
-    await checkoutPage.selectAnyCountry(shippingAddress.country);
-    await checkoutPage.postalCode.fill(shippingAddress.postalCode);
-    await checkoutPage.houseNumber.fill(shippingAddress.houseNumber);
-    await checkoutPage.proceedToCheckoutButton3.click();
-
-    await checkoutPage.selectPaymentMethod('Buy Now Pay Later');
-    await checkoutPage.selectMonthlyInstallments('12 Monthly Installments');
-    await checkoutPage.finishButton.click();
-
-    await expect(checkoutPage.paymentSuccessMessage).toContainText('Payment was successful');
-});
-
 test('check contact form', async ({ mainPage, contactPage }) => {
     const firstName = faker.person.firstName();
     const lastName = faker.person.lastName();
---
+++ tests/e2e-purchase.spec.ts
@@ -0,0 +1,34 @@
+import { faker } from '@faker-js/faker';
+import { test, expect } from '../fixtures/baseTest';
+
+test('e2e purchase flow', async ({ mainPage, productPage, checkoutPage }) => {
+    const zipCode = faker.location.zipCode();
+    const houseNumber = faker.location.buildingNumber();
+    const shippingAddressCountry = 'United States of America (the)';
+
+    await mainPage.open();
+    await mainPage.choseInStock();
+    await productPage.addToCartButton.click();
+    await mainPage.cartLink.click();
+
+    await checkoutPage.proceedToCheckoutButton1.click();
+
+    await checkoutPage.proceedToCheckoutButton2.click();
+
+    await checkoutPage.selectAnyCountry(shippingAddressCountry);
+    await checkoutPage.postalCode.fill(zipCode);
+    await checkoutPage.houseNumber.fill(houseNumber);
+    await checkoutPage.proceedToCheckoutButton3.click();
+
+    await checkoutPage.selectPaymentMethod('Buy Now Pay Later');
+    await checkoutPage.selectMonthlyInstallments('12 Monthly Installments');
+    await checkoutPage.finishButton.click();
+
+    await expect(checkoutPage.paymentSuccessMessage).toContainText('Payment was successful');
+
+    await checkoutPage.finishButton.click();
+
+    await expect(checkoutPage.confirmOrder).toContainText(
+        'Thanks for your order! Your invoice number is',
+    );
+});
