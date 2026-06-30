77afac7558b93ec4e16b417336e102298ddd574c
Eugene <eakvyatkovsky@gmail.com>
2026-06-30 22:24:46 +0400

    test: add mock for successful e2e purchase with custom invoice

--- tests/e2e-purchase.spec.ts
+++ tests/e2e-purchase.spec.ts
@@ -70,3 +70,45 @@ test('should handle payment gateway 500 error gracefully', async ({
 
     await expect(checkoutPage.paymentErrorMessage).toContainText('Internal Server Error');
 });
+
+test('e2e fake invoice flow', async ({ mainPage, productPage, checkoutPage, page }) => {
+    const zipCode = faker.location.zipCode();
+    const houseNumber = faker.location.buildingNumber();
+    const shippingAddressCountry = 'United States of America (the)';
+
+    await mainPage.open();
+    await mainPage.choseInStock();
+    await productPage.addToCartButton.click();
+    await mainPage.cartLink.click();
+
+    await checkoutPage.proceedToCheckoutButton1.click();
+
+    await checkoutPage.proceedToCheckoutButton2.click();
+
+    await checkoutPage.selectAnyCountry(shippingAddressCountry);
+    await checkoutPage.postalCode.fill(zipCode);
+    await checkoutPage.houseNumber.fill(houseNumber);
+    await checkoutPage.proceedToCheckoutButton3.click();
+
+    await checkoutPage.selectPaymentMethod('Buy Now Pay Later');
+    await checkoutPage.selectMonthlyInstallments('12 Monthly Installments');
+    await checkoutPage.finishButton.click();
+
+    await expect(checkoutPage.paymentSuccessMessage).toContainText('Payment was successful');
+
+    await page.route('**/invoices', async (route) => {
+        await route.fulfill({
+            status: 201,
+            contentType: 'application/json',
+            body: JSON.stringify({
+                invoice_number: 'INV-UR-MILLIONER',
+            }),
+        });
+    });
+
+    await checkoutPage.finishButton.click();
+
+    await expect(checkoutPage.confirmOrder).toContainText(
+        'Thanks for your order! Your invoice number is INV-UR-MILLIONER',
+    );
+});
